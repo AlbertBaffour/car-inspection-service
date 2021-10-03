a527f0779f4740c326933a54f763c6c88a61b44c
Albert Baffour Awuah <baffour006@gmail.com>
2021-10-03 12:37:02 +0200

    first commit

---
+++ src/main/java/project/apt/carinspectionservice/CarInspectionServiceApplication.java
@@ -0,0 +1,13 @@
+package project.apt.carinspectionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarInspectionServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarInspectionServiceApplication.class, args);
+    }
+
+}
